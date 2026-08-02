c7df2603717fb3dbbaa36dec7568eb450393c3a4
HostPanel Dev <dev@hostpanel.local>
2026-08-02 15:32:22 +0700

    Remove dev.db and scripts

--- check.js
+++
@@ -1,5 +0,0 @@
-const sqlite3 = require('sqlite3');
-const db = new sqlite3.Database('./dev.db');
-db.all('SELECT id, name, port FROM Application', (err, rows) => {
-  console.log(JSON.stringify(rows, null, 2));
-});
--- check_db.js
+++
@@ -1,3 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-prisma.application.findMany().then(apps => console.log(JSON.stringify(apps, null, 2))).finally(() => prisma.$disconnect());
